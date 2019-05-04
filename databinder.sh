1d688c0bea36a76769cfc549e1facc9bd356d03d
Carlo Marinangeli <carlo.marinangeli@gmail.com>
2019-05-05 02:51:09 +0400

    fixing script

--- databinder.sh
+++ databinder.sh
@@ -2,12 +2,14 @@ set -x
 
 rm -rf build-databinding
 
+LAYOUT_INFO_OUTPUT='build-databinding/layoutinfo'
+RES_OUTPUT='build-databinding/resOutput'
+
 mkdir build-databinding
-mkdir build-databinding/layoutinfo
-mkdir build-databinding/resOutput
+mkdir $RES_OUTPUT
+mkdir $LAYOUT_INFO_OUTPUT
+mkdir aarOutDir  # used by buck processor
 
-
-LAYOUT_INFO_OUTPUT='/Users/carlo/Projects/BuckApp/build-databinding/layoutinfo'
 PACKAGE='com.hackathon.buckapp'
 
 
@@ -15,7 +17,7 @@ java -jar /Users/carlo/Projects/aosp/tools/data-binding/exec/build/intermediates
 PROCESS \
 -package $PACKAGE \
 -resInput /Users/carlo/Projects/BuckApp/app/src/main/res \
--resOutput /Users/carlo/Projects/BuckApp/build-databinding/resOutput \
+-resOutput $RES_OUTPUT \
 -layoutInfoOutput $LAYOUT_INFO_OUTPUT \
 -zipLayoutInfo true \
 -useAndroidX true
@@ -23,7 +25,7 @@ PROCESS \
 
 java -jar /Users/carlo/Projects/aosp/tools/data-binding/exec/build/intermediates/fullJar/android-data-binding-fat.jar \
 GEN_BASE_CLASSES \
--classInfoOut /Users/carlo/Projects/BuckApp/build-databinding/class-info-out \
+-classInfoOut /Users/carlo/Projects/BuckApp/build-databinding/class-info-out.zip \
 -layoutInfoFiles $LAYOUT_INFO_OUTPUT/layout-info.zip \
 -package $PACKAGE \
 -sourceOut /Users/carlo/Projects/BuckApp/build-databinding/sourceOut.zip \
